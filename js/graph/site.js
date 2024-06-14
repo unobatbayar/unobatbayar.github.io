bd5db0712c6a88ea451ea457aed6366fd428b0ed
Usukhbayar Batbayar <38866249+unobatbayar@users.noreply.github.com>
2024-06-14 23:13:05 +0900

    Update site.js

--- js/graph/site.js
+++ js/graph/site.js
@@ -35,12 +35,12 @@ nodes:{
   // interests
   interest1:{'color':'cornflowerblue','shape':'square','label':'Apps'},
   interest2:{'color':'teal','shape':'square','label':'Chess'},
-  interest3:{'color':'tomato','shape':'square','label':'Running'}
-  // interest4:{'color':'darkslategrey','shape':'square','label':''},
+  interest3:{'color':'tomato','shape':'square','label':'Running'},
+  interest4:{'color':'darkslategrey','shape':'square','label':'Piano'},
 
 },
 edges:{
-  interests:{interest1:{}, interest2:{}, interest3:{}},
+  interests:{interest1:{}, interest2:{}, interest3:{}, interest4:{}},
   programming:{lang1:{}, lang2:{}, lang3:{}},
 }
 };
