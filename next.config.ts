c3e9ffdf16565cda371e5b00d13e2f0c2da37c7c
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2025-01-10 07:42:03 +0900

    final test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 // next.config.js
 module.exports = {
   // Ensure the app is ready for static export
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,  // Optional: Disable image optimization if needed
   },
