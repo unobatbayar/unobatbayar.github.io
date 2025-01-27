f21e95b5255cee807518a3acc50b77ed5c764573
Usukhbayar Batbayar <38866249+unobatbayar@users.noreply.github.com>
2025-01-27 12:18:34 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -25,10 +25,7 @@ export default function Page() {
         Hello!👋 I'm Usukhbayar Batbayar, but you can call me Uno. I'm a software engineer based in Japan, originally from Ulaanbaatar, Mongolia. 
       </p>
       <p>
-        I started my career focusing on mobile app development, and recently, I've been learning how to build web applications.
-      </p>
-      <p>
-        Lately, I've also developed an interest in quantitative trading and have been exploring it as a hobby in my free time. 
+        I started my career focusing on mobile app development, and recently, I've been learning how to build web applications. I've also developed an interest in quantitative trading and have been exploring it as a hobby in my free time. 
       </p>
 <p>
 I'm excited to deepen my skills in both software engineering and quantitative trading. 
