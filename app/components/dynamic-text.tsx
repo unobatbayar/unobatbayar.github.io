f4125a6ba5566ffdce189c38c759058700a46086
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2025-01-31 15:37:26 +0900

    fix: increase to 7 seconds

--- app/components/dynamic-text.tsx
+++ app/components/dynamic-text.tsx
@@ -7,22 +7,22 @@ const DynamicText = () => {
     'building apps',
     'designing websites',
     'playing chess',
-    'analyzing financial markets', 
+    'analyzing financial markets',
     'cryptocurrency',
     'security and privacy',
     'going for a run'
   ];
 
   // Corresponding colors for each theme
-const colors = [
-  'text-blue-500',       // Building apps (technology) - Bright and modern
-  'text-lime-500',       // Websites (creativity, calm) - Light and fresh
-  'text-green-500',      // Chess (strategy, intellectual) - Bold and energetic
-  'text-yellow-500',     // Finance (growth, stability) - Fresh and stable
-  'text-purple-500',     // Cryptocurrency (innovation) - Bold and futuristic
-  'text-red-500',        // Privacy & Security (alert, security) - Strong and attention-grabbing
-  'text-orange-500'      // Running (energy, vitality) - Bright and energetic
-];
+  const colors = [
+    'text-blue-500',       // Building apps (technology) - Bright and modern
+    'text-lime-500',       // Websites (creativity, calm) - Light and fresh
+    'text-green-500',      // Chess (strategy, intellectual) - Bold and energetic
+    'text-yellow-500',     // Finance (growth, stability) - Fresh and stable
+    'text-purple-500',     // Cryptocurrency (innovation) - Bold and futuristic
+    'text-red-500',        // Privacy & Security (alert, security) - Strong and attention-grabbing
+    'text-orange-500'      // Running (energy, vitality) - Bright and energetic
+  ];
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
   const [displayText, setDisplayText] = useState(texts[0]);
   const [currentColor, setCurrentColor] = useState(colors[0]);
@@ -31,7 +31,7 @@ const colors = [
     const interval = setInterval(() => {
       setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);  // Loop through texts
       setCurrentColor(colors[(currentTextIndex + 1) % colors.length]); // Loop through colors
-    }, 3000); // Change text and color every n seconds
+    }, 7000); // Change text and color every n seconds
 
     return () => clearInterval(interval);  // Clean up the interval when component unmounts
   }, [currentTextIndex]);
