6d26299bdb37b65a4e10e2f239021c0bf25dd122
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2025-09-24 10:45:54 +0900

    fix: link

--- app/config.ts
+++ app/config.ts
@@ -12,7 +12,7 @@ export const metaData = {
 
 export const socialLinks = {
   appstore: "https://apps.apple.com/developer/usukhbayar-batbayar/id1532655863",
-  github: "https://github.com/x1vfk7wp13",
+  github: "https://github.com/unobatbayar",
   stackoverflow: "https://stackoverflow.com/users/12330629",
   linkedin: "https://www.linkedin.com/in/rytx88jz24dm",
   leetcode: "https://leetcode.com/u/xi76ibaw2m/"
