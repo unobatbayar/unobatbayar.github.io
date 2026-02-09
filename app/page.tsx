8b5a51802dee298284d5d44f59f92318c8690680
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2026-02-09 17:07:38 -0500

    docs: update

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <section>
       <p className="text-2xl font-medium text-black dark:text-white mt-0 pt-0">
-        Hello! I'm Uno 👋, a sofware engineer from <a className="text-blue-500" href="https://en.wikipedia.org/wiki/Ulaanbaatar">Ulaanbaatar, Mongolia</a>. Interested in privacy and security.
+        Hello! I'm Uno 👋, a software engineer from <a className="text-blue-500" href="https://en.wikipedia.org/wiki/Ulaanbaatar">Ulaanbaatar, Mongolia</a>. Interested in privacy and AI-driven developer tools.
       </p>
 
 
