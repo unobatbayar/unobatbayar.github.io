8670a789768c3e6d8aa945c2d0eb1bdb4869f1d8
unobatbayar <38866249+unobatbayar@users.noreply.github.com>
2026-03-05 21:40:13 -0500

    fix: remove twitter

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -6,7 +6,6 @@ import {
   FaGithub,
   FaLinkedinIn,
   FaStackOverflow,
-  FaTwitter
 } from "react-icons/fa6";
 
 const navItems = {
@@ -57,11 +56,6 @@ export function Navbar() {
               href={socialLinks.appstore} 
               icon={FaAppStore} 
               className="text-[#007AFF] hover:text-[#0051D5] dark:text-[#0a84ff] dark:hover:text-[#409cff]"
-            />
-            <SocialLink 
-              href={socialLinks.twitter} 
-              icon={FaTwitter} 
-              className="text-[#1DA1F2] hover:text-[#0c85d6] dark:text-[#1DA1F2] dark:hover:text-[#66b3ff]"
             />
           </div>
         </div>
--- app/config.ts
+++ app/config.ts
@@ -7,7 +7,7 @@ export const metaData = {
     "Personal website of Usukhbayar Batbayar, a software developer passionate about web development, mobile apps, and martial arts. Explore my projects, blog posts, and journey in technology and Judo.",
   keywords: "Usukhbayar Batbayar, software developer, web development, mobile apps, React, Next.js, JavaScript, TypeScript, Judo, martial arts, programming, technology",
   author: "Usukhbayar Batbayar",
-  twitterHandle: "@unobatbayar",
+  twitterHandle: "",
 };
 
 export const socialLinks = {
@@ -15,5 +15,4 @@ export const socialLinks = {
   github: "https://github.com/unobatbayar",
   stackoverflow: "https://stackoverflow.com/users/12330629",
   linkedin: "https://www.linkedin.com/in/rytx88jz24dm",
-  twitter: "https://www.x.com/unobatbayar"
 };
